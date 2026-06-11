55ef86801ecb813d2d9053162f33a6e3aec556b9
Nathan Castro <nathanslc18@gmail.com>
2026-06-11 13:19:25 -0300

    Update __init__.py

--- src/scrapers/__init__.py
+++ src/scrapers/__init__.py
@@ -1,4 +1,4 @@
-from .skyscanner import SkyscannerScraper
-from .google_flights import GoogleFlightsScraper
+from .skyscanner import scrape as skyscanner_scrape
+from .google_flights import scrape as google_flights_scrape
 
-__all__ = ["SkyscannerScraper", "GoogleFlightsScraper"]
+__all__ = ["skyscanner_scrape", "google_flights_scrape"]
